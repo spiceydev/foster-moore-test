1920ac215d40112e4633e32507abea3bc0456110
spiceydev <spiceydev@gmail.com>
2022-01-26 13:48:18 +1300

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react";
 
 const Layout: FC = ({ children }) => {
-  return <div className="container mx-auto mt-8">{children}</div>;
+  return <div className="container m-8 mx-auto">{children}</div>;
 };
 
 export default Layout;
